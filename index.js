abd96bc7fca2a7b13cde5382d4df84f702ef2dfe
cory-p-oncota <coryparent@oncota.com>
2015-07-26 21:06:49 +0000

    html service

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ var fs = require('fs');
 
 app.get('/', function (req, res) {
   fs.readFile('/clockwork/read.dat', function (err, dat) {
-    res.send({
-      state: ((dat === 0) ? 'open' : 'closed')
-    });
+    var color = (dat.toString() == 0 ? '#0c0' : '#f00');
+    var state = (date.toString() == 0 ? 'open!' : 'closed...');
+
+    var response = '<div style="color:#fff; padding:20px; background:' + color + '; width:200px; height:200px;">Restroom is ' + state + '</div>';
+
+    res.send(response);
   });
 });
 
